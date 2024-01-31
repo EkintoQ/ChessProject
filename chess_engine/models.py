fd352a9d82acfe7e0b4347dbda0864acdea1f1bf
EkintoQ <ekinto88@gmail.com>
2024-01-31 15:05:49 +0100

    fix

--- chess_engine/models.py
+++ chess_engine/models.py
@@ -4,21 +4,48 @@ import chess
 # from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-from users.models import User
+from users.models import CustomUser
 
 
-class ChessGame(models.Model):
+class BaseChessGame(models.Model):
     fen = models.CharField(max_length=255)
     game_id = models.AutoField(primary_key=True)
-    player = models.ForeignKey(User, on_delete=models.CASCADE)
+    player = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        help_text="Customer account who played this game.",
+    )
 
+    class Meta:
+        abstract = True
+
+
+class BotChessGame(BaseChessGame):
     @classmethod
-    def create_new_game(cls):
-        new_game = cls(fen=chess.STARTING_FEN)
+    def create_new_game(cls, player):
+        new_game = cls(fen=chess.STARTING_FEN, player=player)
+        new_game.save()
+        return new_game
+
+
+class SelfChessGame(BaseChessGame):
+    @classmethod
+    def create_new_game(cls, player):
+        new_game = cls(fen=chess.STARTING_FEN, player=player)
+        new_game.save()
+        return new_game
+
+
+class MultiplayerChessGame(BaseChessGame):
+    @classmethod
+    def create_new_game(cls, player):
+        new_game = cls(fen=chess.STARTING_FEN, player=player)
         new_game.save()
         return new_game
 
 
 class Move(models.Model):
-    game = models.ForeignKey(ChessGame, on_delete=models.CASCADE)
+    # content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    # object_id = models.PositiveIntegerField()
+    # game = GenericForeignKey('content_type', 'object_id')
     move_text = models.CharField(max_length=10)
